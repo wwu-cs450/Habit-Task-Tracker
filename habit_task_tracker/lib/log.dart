901d503178d151b9416a221ea888945bc012cf1c
keegan <keegan.sand@gmail.com>
2025-11-05 20:31:42 -0800

    Added Log class

---
+++ habit_task_tracker/lib/log.dart
@@ -0,0 +1,54 @@
+import 'package:habit_task_tracker/backend.dart';
+
+class Log {
+  final String _id;
+  DateTime timestamp;
+  String habitId;
+  String? notes;
+
+  Log({
+    required String id,
+    required this.timestamp,
+    required this.habitId,
+    this.notes,
+  }) : _id = id;
+
+  String get gId => _id;
+
+  DateTime get gTimestamp => timestamp;
+
+  String get gHabitId => habitId;
+
+  String? get gNotes => notes;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': _id,
+      'timestamp': timestamp.toIso8601String(),
+      'habitId': habitId,
+      'notes': notes,
+    };
+  }
+
+  factory Log.fromJson(Map<String, dynamic> json) {
+    return Log(
+      id: json['id'],
+      timestamp: DateTime.parse(json['timestamp']),
+      habitId: json['habitId'],
+      notes: json['notes'],
+    );
+  }
+}
+
+int saveLog(Log log) {
+  saveData('Logs', log._id.toString(), log.toJson());
+  return 0;
+}
+
+Future<Log> loadLog(int id) async {
+  var data = await loadData('Logs', id.toString());
+  if (data == null) {
+    throw Exception('Log with id $id not found');
+  }
+  return Log.fromJson(data);
+}
