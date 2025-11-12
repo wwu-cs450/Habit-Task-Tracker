7a044249bf3a67ac98d3dc3c704c107cd498c70d
Joshua Garbi <joshua.garbi@wallawalla.edu>
2025-11-12 12:02:33 -0800

    add searching with fuzzywuzzy

---
+++ habit_task_tracker/lib/search.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/cupertino.dart';
+import 'package:habit_task_tracker/habit.dart';
+// import 'package:habit_task_tracker/log.dart';
+import 'package:localstore/localstore.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
+
+
+final db = Localstore.instance;
+final fuzzyVal = 70; // threshold for fuzzy search
+
+// check if //data/habits exists
+final filePath = Directory('/data/habits');
+bool exists = filePath.existsSync();
+final collectionHabits = exists? db.collection('data/habits') : null;
+
+List<Habit> searchHabitsBetweenDates(DateTime date1, DateTime date2) {
+  List<Habit> results = [];
+  if (!exists) {
+    print('No habits found in the database.');
+    return results;
+  }
+  collectionHabits?.get().then((habitsData) {
+    if (habitsData != null) {
+      habitsData.forEach((id, data) {
+        Habit habit = Habit.fromJson(data);
+        if ((habit.startDate.isAfter(date1) || habit.startDate.isAtSameMomentAs(date1)) &&
+            (habit.startDate.isBefore(date2) || habit.startDate.isAtSameMomentAs(date2))) {
+          results.add(habit);
+        }
+      });
+
+    }
+  });
+  return results;
+}
+
+List<Habit> searchHabitsByName(String name) {
+  List<Habit> results = [];
+  if (!exists) {
+    print('No habits found in the database.');
+    return results;
+  }
+  collectionHabits?.get().then((habitsData) {
+    if (habitsData != null) {
+      habitsData.forEach((id, data) {
+        Habit habit = Habit.fromJson(data);
+        int ratioName = ratio(habit.name.toLowerCase(), name.toLowerCase());
+        if (ratioName > fuzzyVal) {
+          results.add(habit);
+        }
+      });
+    }
+  });
+  return results;
+}
+
+List<Habit> searchHabitsByDescription(String description) {
+  List<Habit> results = [];
+  if (!exists) {
+    print('No habits found in the database.');
+    return results;
+  }
+  collectionHabits?.get().then((habitsData) {
+    if (habitsData != null) {
+      habitsData.forEach((id, data) {
+        Habit habit = Habit.fromJson(data);
+        if (habit.description != null) {
+          int ratioDesc = ratio(habit.description!.toLowerCase(), description.toLowerCase());
+          if (ratioDesc > fuzzyVal) {
+            results.add(habit);
+          }
+        }
+      });
+    }
+  });
+  return results;
+}
+
