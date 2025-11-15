bc69215490ef5536bba815c9da0ae6c7950ba499
Joshua Garbi <joshua.garbi@gmail.com>
2025-11-14 17:53:01 -0800

    fixed async problems

--- habit_task_tracker/lib/search.dart
+++ habit_task_tracker/lib/search.dart
@@ -15,85 +15,88 @@ final filePath = Directory('data/Habits');
 bool exists = filePath.existsSync();
 final collectionHabits = exists? db.collection('data/Habits') : null;
 
-List<Habit> searchHabits({DateTime? date1, DateTime? date2, String? name, String? description}) {
-  List<Habit> results = [];
+Future<List<Habit>> searchHabits({DateTime? date1, DateTime? date2, String? name, String? description}) {
+  Future<List<Habit>> results = Future.value([]);
   if (!exists) {
     print('No habits found in the database. in $filePath');
     return results;
   }
   if (date1 != null && date2 != null) {
-    results.addAll(searchHabitsBetweenDates(date1, date2));
+    return searchHabitsBetweenDates(date1, date2);
   }
   else if (name != null) {
-    results.addAll(searchHabitsByName(name));
+    return searchHabitsByName(name);
   }
   else if (description != null) {
-    results.addAll(searchHabitsByDescription(description));
+    return searchHabitsByDescription(description);
   }
   return results;
 }
 
 
-List<Habit> searchHabitsBetweenDates(DateTime date1, DateTime date2) {
+Future<List<Habit>> searchHabitsBetweenDates(DateTime date1, DateTime date2) async {
   List<Habit> results = [];
   if (!exists) {
     print('No habits found in the database.');
     return results;
   }
-  collectionHabits?.get().then((habitsData) {
-    if (habitsData != null) {
-      habitsData.forEach((id, data) {
-        Habit habit = Habit.fromJson(data);
-        if ((habit.startDate.isAfter(date1) || habit.startDate.isAtSameMomentAs(date1)) &&
-            (habit.startDate.isBefore(date2) || habit.startDate.isAtSameMomentAs(date2))) {
+  final habitsData = await collectionHabits?.get();
+  if (habitsData == null) {
+    print('No habits data found.');
+    return results;
+  };
+  habitsData.forEach((id, data) {
+    Habit habit = Habit.fromJson(data);
+    if ((habit.startDate.isAfter(date1) || habit.startDate.isAtSameMomentAs(date1)) &&
+        (habit.endDate.isBefore(date2) || habit.endDate.isAtSameMomentAs(date2))) {
           results.add(habit);
-        }
-      });
-
     }
-  });
+  });  
   return results;
 }
 
-List<Habit> searchHabitsByName(String name) {
+Future<List<Habit>> searchHabitsByName(String name) async {
   List<Habit> results = [];
   if (!exists) {
     print('No habits found in the database.');
     return results;
   }
-  collectionHabits?.get().then((habitsData) {
-    if (habitsData != null) {
-      habitsData.forEach((id, data) {
-        Habit habit = Habit.fromJson(data);
-        int ratioName = ratio(habit.name.toLowerCase(), name.toLowerCase());
-        if (ratioName > fuzzyVal) {
-          results.add(habit);
-        }
-      });
+  final habitsData = await collectionHabits?.get();
+  if (habitsData == null) {
+    print('No habits data found.');
+    return results;
+  };
+  habitsData.forEach((id, data) {
+    Habit habit = Habit.fromJson(data);
+    int ratioName = ratio(habit.name.toLowerCase(), name.toLowerCase());
+    if (ratioName > fuzzyVal) {
+      results.add(habit);
     }
   });
   return results;
 }
 
-List<Habit> searchHabitsByDescription(String description) {
+Future<List<Habit>> searchHabitsByDescription(String description) async {
   List<Habit> results = [];
   if (!exists) {
     print('No habits found in the database.');
     return results;
   }
-  collectionHabits?.get().then((habitsData) {
-    if (habitsData != null) {
-      habitsData.forEach((id, data) {
-        Habit habit = Habit.fromJson(data);
-        if (habit.description != null) {
-          int ratioDesc = ratio(habit.description!.toLowerCase(), description.toLowerCase());
-          if (ratioDesc > fuzzyVal) {
-            results.add(habit);
-          }
-        }
-      });
+  final habitsData = await collectionHabits?.get();
+  if (habitsData == null) {
+    print('No habits data found.');
+    return results;
+  };
+  habitsData.forEach((id, data) {
+    Habit habit = Habit.fromJson(data);
+    if (habit.description != null) {
+      int ratioDesc = partialRatio(habit.description!.toLowerCase(), description.toLowerCase());
+      if (ratioDesc > fuzzyVal) {
+        results.add(habit);
+      }
     }
   });
+  
   return results;
 }
 
