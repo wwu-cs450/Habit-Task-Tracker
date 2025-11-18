6c50a885fcdf45c1fecb8a36b7c9357c6e67ec22
Joshua Garbi <joshua.garbi@gmail.com>
2025-11-17 17:57:56 -0800

    adding search for all habits

--- habit_task_tracker/lib/search.dart
+++ habit_task_tracker/lib/search.dart
@@ -6,16 +6,20 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 
-
 final db = Localstore.instance;
 final fuzzyVal = 70; // threshold for fuzzy search
 
 // check if //data/habits exists
 final filePath = Directory('data/Habits');
 bool exists = filePath.existsSync();
-final collectionHabits = exists? db.collection('data/Habits') : null;
+final collectionHabits = exists ? db.collection('data/Habits') : null;
 
-Future<List<Habit>> searchHabits({DateTime? date1, DateTime? date2, String? name, String? description}) {
+Future<List<Habit>> searchHabits({
+  DateTime? date1,
+  DateTime? date2,
+  String? name,
+  String? description,
+}) {
   Future<List<Habit>> results = Future.value([]);
   if (!exists) {
     print('No habits found in the database. in $filePath');
@@ -23,18 +27,24 @@ Future<List<Habit>> searchHabits({DateTime? date1, DateTime? date2, String? name
   }
   if (date1 != null && date2 != null) {
     return searchHabitsBetweenDates(date1, date2);
-  }
-  else if (name != null) {
+  } else if (name != null) {
     return searchHabitsByName(name);
-  }
-  else if (description != null) {
+  } else if (description != null) {
     return searchHabitsByDescription(description);
+  } else if (date1 == null &&
+      date2 == null &&
+      name == null &&
+      description == null) {
+    return searchAllHabits();
+    return results;
   }
   return results;
 }
 
-
-Future<List<Habit>> searchHabitsBetweenDates(DateTime date1, DateTime date2) async {
+Future<List<Habit>> searchHabitsBetweenDates(
+  DateTime date1,
+  DateTime date2,
+) async {
   List<Habit> results = [];
   if (!exists) {
     print('No habits found in the database.');
@@ -44,14 +54,17 @@ Future<List<Habit>> searchHabitsBetweenDates(DateTime date1, DateTime date2) asy
   if (habitsData == null) {
     print('No habits data found.');
     return results;
-  };
+  }
+  ;
   habitsData.forEach((id, data) {
     Habit habit = Habit.fromJson(data);
-    if ((habit.startDate.isAfter(date1) || habit.startDate.isAtSameMomentAs(date1)) &&
-        (habit.endDate.isBefore(date2) || habit.endDate.isAtSameMomentAs(date2))) {
-          results.add(habit);
+    if ((habit.startDate.isAfter(date1) ||
+            habit.startDate.isAtSameMomentAs(date1)) &&
+        (habit.endDate.isBefore(date2) ||
+            habit.endDate.isAtSameMomentAs(date2))) {
+      results.add(habit);
     }
-  });  
+  });
   return results;
 }
 
@@ -65,7 +78,8 @@ Future<List<Habit>> searchHabitsByName(String name) async {
   if (habitsData == null) {
     print('No habits data found.');
     return results;
-  };
+  }
+  ;
   habitsData.forEach((id, data) {
     Habit habit = Habit.fromJson(data);
     int ratioName = ratio(habit.name.toLowerCase(), name.toLowerCase());
@@ -86,17 +100,38 @@ Future<List<Habit>> searchHabitsByDescription(String description) async {
   if (habitsData == null) {
     print('No habits data found.');
     return results;
-  };
+  }
+  ;
   habitsData.forEach((id, data) {
     Habit habit = Habit.fromJson(data);
     if (habit.description != null) {
-      int ratioDesc = partialRatio(habit.description!.toLowerCase(), description.toLowerCase());
+      int ratioDesc = partialRatio(
+        habit.description!.toLowerCase(),
+        description.toLowerCase(),
+      );
       if (ratioDesc > fuzzyVal) {
         results.add(habit);
       }
     }
   });
-  
+
   return results;
 }
 
+Future<List<Habit>> searchAllHabits() async {
+  List<Habit> results = [];
+  if (!exists) {
+    print('No habits found in the database.');
+    return results;
+  }
+  final habitsData = await collectionHabits?.get();
+  if (habitsData == null) {
+    print('No habits data found.');
+    return results;
+  }
+  habitsData.forEach((id, data) {
+    Habit habit = Habit.fromJson(data);
+    results.add(habit);
+  });
+  return results;
+}
